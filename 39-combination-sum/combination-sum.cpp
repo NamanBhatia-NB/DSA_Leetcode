e3eb8b6013b86111dfc6ce8e1dce32b080f25662
Naman Bhatia <145786560+NamanBhatia-NB@users.noreply.github.com>
2026-05-24 13:33:42 +0530

    Time: 79 ms (8.03%) | Memory: 71.2 MB (6.45%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void solve(vector<int>& candidates, int target, int i, int sum, vector<int> combination, vector<vector<int>>& ans) {
+        if(sum==target){
+            ans.push_back(combination);
+            return;
+        }
+
+        if( i >= candidates.size() || sum>target){
+            return;
+        }
+
+        // include
+        combination.push_back(candidates[i]);
+        solve(candidates, target, i, sum + candidates[i], combination, ans);
+
+        // exclude
+        combination.pop_back();
+        solve(candidates, target, i + 1, sum, combination, ans);
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        solve(candidates, target, 0, 0, {}, ans);
+
+        return ans;
+    }
+};
