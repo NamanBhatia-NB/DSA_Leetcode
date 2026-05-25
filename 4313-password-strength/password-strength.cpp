645701d258de6294d8b13b06fd8c81f653711e5b
Naman Bhatia <145786560+NamanBhatia-NB@users.noreply.github.com>
2026-05-26 01:16:26 +0530

    Time: 2 ms (100.00%) | Memory: 15.9 MB (40.00%) - LeetSync

--- 4313-password-strength/password-strength.cpp
+++ 4313-password-strength/password-strength.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
     int passwordStrength(string password) {
-        unordered_set<char> s;
+        vector<bool> upper(26, false);
+        vector<bool> lower(26, false);
+        vector<bool> num(10, false);
+        unordered_set<char> spl;
 
-        for(char c : password){
-            s.insert(c);
-        }
         int ans = 0;
-        for(char c : s){
-            if((c-'0')>=0 && (c-'0')<=9)
-                ans+=3;
-            else if((c-'a')>=0 && (c-'z')<=0){
-                ans +=1;
+        for (char c : password) {
+            if (c >= '0' && c <= '9') {
+                if (!num[c - '0']) {
+                    num[c - '0'] = true;
+                    ans += 3;
+                }
+            } else if (c >= 'a' && c <= 'z') {
+                if (!lower[c - 'a']) {
+                    lower[c - 'a'] = true;
+                    ans += 1;
+                }
+            } else if (c >= 'A' && c <= 'Z') {
+                if (!upper[c - 'A']) {
+                    upper[c - 'A'] = true;
+                    ans += 2;
+                }
+            } else if (!spl.count(c)) {
+                spl.insert(c);
+                ans += 5;
             }
-            else if((c-'A')>=0 && (c-'Z')<=0){
-                ans+=2;
-            }
-            else{
-                ans+=5;
-            }
-            cout<<ans<<" ";
         }
 
         return ans;
